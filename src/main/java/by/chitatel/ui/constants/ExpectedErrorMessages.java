ad20e40d3d7cf8d4bc60caed4ab039aeadbabc8c
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-05 12:14:31 +0200

    [ADD] Added class ExpectedErrorMessages

---
+++ src/main/java/by/chitatel/ui/constants/ExpectedErrorMessages.java
@@ -0,0 +1,13 @@
+package by.chitatel.ui.constants;
+
+public abstract class ExpectedErrorMessages {
+    public static final String PHONE_NOT_FOUND = "Такой телефон не найден";
+    public static final String PASSWORD_SEND_TO_PHONE_NUMBER = "Пароль отправлен на номер %s";
+    public static final String PASSWORD_DOES_NOT_MATCH = "Пароль не совпадает";
+    public static final String PASSWORD_IS_TOO_LONG = "The password phone may not be greater than 255 characters.";
+    public static final String PHONE_NUMBER_WAS_NOT_INPUT = "Вы не указали \"Телефон\"";
+    public static final String PASSWORD_WAS_NOT_INPUT = "Вы не указали \"Пароль\"";
+    public static final String EMAIL_WAS_NOT_INPUT = "Вы не указали \"Email\"";
+    public static final String EMAIL_AND_PASSWORD_ARE_WRONG = "Неправильный e-mail или пароль!";
+    public static final String EMAIL_AND_PASSWORD_WERE_NOT_INPUT = "Вы не указали \"Email\"\nВы не указали \"Пароль\"";
+}
