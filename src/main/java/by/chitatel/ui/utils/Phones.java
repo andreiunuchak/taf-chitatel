aa18a3704ffd8c0f5ab8ad4f28dc552a1bf2d3c0
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-05 12:13:44 +0200

    [ADD] Added util class Phones

---
+++ src/main/java/by/chitatel/ui/utils/Phones.java
@@ -0,0 +1,41 @@
+package by.chitatel.ui.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Phones {
+    public static String getCorrectCountryCode() {
+        return "+375";
+    }
+
+    public static String getCorrectOperatorCode() {
+        String[] validOperatorCodes = {"29", "33", "44", "25"};
+        return validOperatorCodes[(int) (Math.random() * validOperatorCodes.length)];
+    }
+
+    public static String generateIncorrectOperatorCode() {
+        List<String> validOperatorCodes = new ArrayList<>(Arrays.asList("29", "33", "44", "25"));
+        String incorrectOperatirCode = "";
+        do {
+            incorrectOperatirCode = "";
+            incorrectOperatirCode += (int) ((Math.random() * 90) + 10);
+        } while (validOperatorCodes.contains(incorrectOperatirCode));
+        return incorrectOperatirCode;
+    }
+
+    public static String getMockedPhoneNumber() {
+        return "0000000";
+    }
+
+    public static String generatePhoneNumber(int length) {
+        if (length <= 0) {
+            return "";
+        }
+        StringBuilder phoneNumber = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            phoneNumber.append((int) (Math.random() * 10));
+        }
+        return phoneNumber.toString();
+    }
+}
