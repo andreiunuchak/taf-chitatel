1d9fd14f5322a5e376f9bb483e068a4952c31c5c
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-05 15:51:49 +0200

    [ADD] Added Phone Object class

---
+++ src/main/java/by/chitatel/ui/utils/objects/Phone.java
@@ -0,0 +1,48 @@
+package by.chitatel.ui.utils.objects;
+
+import java.util.Objects;
+
+public class Phone {
+    private final String countryCode;
+    private final String operatorCode;
+    private final String phoneNumber;
+
+    public Phone(String countryCode, String operatorCode, String phoneNumber) {
+        this.countryCode = countryCode;
+        this.operatorCode = operatorCode;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getOperatorCode() {
+        return operatorCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPhoneNumberWithOperatorCode() {
+        return operatorCode + phoneNumber;
+    }
+
+    public String getFullPhoneNumber() {
+        return countryCode + operatorCode + phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Phone that = (Phone) o;
+        return Objects.equals(countryCode, that.countryCode) && Objects.equals(operatorCode, that.operatorCode) && Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(countryCode, operatorCode, phoneNumber);
+    }
+}
