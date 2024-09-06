69e5704e6ed7664960c78d46f87433a79e38c6c7
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-06 13:41:31 +0200

    [ADD] Added method without params [REFACTOR] Usage Random instead of Math

--- src/main/java/by/chitatel/ui/utils/Passwords.java
+++ src/main/java/by/chitatel/ui/utils/Passwords.java
@@ -1,23 +1,31 @@
 package by.chitatel.ui.utils;
 
+import java.util.Random;
+
 public class Passwords {
+    private static final Random RANDOM = new Random();
+    private static final char[] ALL_CHARS = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
+            '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
+            '_', '`', '{', '|', '}', '~'
+    };
+
+    public static String generatePassword() {
+        return generatePassword(RANDOM.nextInt(12) + 8);
+    }
+
     public static String generatePassword(int length) {
         if (length <= 0) {
             return "";
         }
-        char[] allChars = {
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
-                '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
-                '_', '`', '{', '|', '}', '~'
-        };
         StringBuilder password = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            password.append(allChars[(int) (Math.random() * allChars.length)]);
+            password.append(ALL_CHARS[RANDOM.nextInt(ALL_CHARS.length)]);
         }
         return password.toString();
     }
