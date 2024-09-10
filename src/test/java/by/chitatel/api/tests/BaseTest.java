21b35ec0fc1b6ced895bd634975f383634164c60
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-10 15:17:11 +0200

    [ADD] Added BaseTest class

---
+++ src/test/java/by/chitatel/api/tests/BaseTest.java
@@ -0,0 +1,19 @@
+package by.chitatel.api.tests;
+
+import by.chitatel.api.apis.Login;
+import by.chitatel.api.utils.Responses;
+import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTest {
+    protected String csrfToken;
+    protected Cookies cookies;
+
+    @BeforeEach
+    public void getCSRFData() {
+        Response response = new Login().execute();
+        csrfToken = Responses.getCSRFToken(response);
+        cookies = Responses.getCookies(response);
+    }
+}
