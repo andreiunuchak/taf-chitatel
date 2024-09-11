0c5df42d7b8cc146c8469e67bf569851531e2129
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-12 01:49:43 +0200

    [REFACTOR] Waiting increased to 30 sec

--- src/main/java/by/chitatel/ui/utils/Waiters.java
+++ src/main/java/by/chitatel/ui/utils/Waiters.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Waiters {
-    private static final int WAIT_DURATION_SEC = 10;
+    private static final int WAIT_DURATION_SEC = 30;
 
     public static WebElement waitForElementPresence(WebDriver driver, By by) {
         return new WebDriverWait(driver, Duration.ofSeconds(WAIT_DURATION_SEC)).until(ExpectedConditions.presenceOfElementLocated(by));
