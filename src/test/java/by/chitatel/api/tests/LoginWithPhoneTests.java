3f3c3279442fa987f100aa8c419ac758da3a72e7
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-12 23:55:00 +0200

    [REFACTOR] Changed way of creating formParameters

--- src/test/java/by/chitatel/api/tests/LoginWithPhoneTests.java
+++ src/test/java/by/chitatel/api/tests/LoginWithPhoneTests.java
@@ -3,17 +3,20 @@ package by.chitatel.api.tests;
 import by.chitatel.api.apis.LoginWithPhone;
 import by.chitatel.api.responses.login.phone.PhoneLoginErrors;
 import by.chitatel.api.utils.Errors;
+import by.chitatel.api.utils.FormParameters;
 import by.chitatel.api.utils.Responses;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 public class LoginWithPhoneTests extends BaseTest {
 
     @Test
     public void testChitatelPhoneLogin() {
-        Response response = new LoginWithPhone("+375(99)1111111", "test", true, csrfToken, cookies).execute();
-
+        Map<String, Object> formParams = new FormParameters().setPhone("+375(99)1111111").setPhonePassword("test").setPhoneRememberMe("1").build();
+        Response response = new LoginWithPhone().performRequestWithParamsTokenCookies(formParams, csrfToken, cookies);
         int statusCode = Responses.getStatusCodeFromResponse(response);
         PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
 
