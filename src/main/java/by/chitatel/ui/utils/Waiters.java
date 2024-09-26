64c2dd1939df2831a0fbd0637421e509aa1a221e
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-26 14:47:51 +0200

    [ADD] Added waiter to return list of elements

--- src/main/java/by/chitatel/ui/utils/Waiters.java
+++ src/main/java/by/chitatel/ui/utils/Waiters.java
@@ -7,12 +7,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Waiters {
     private static final int WAIT_DURATION_SEC = 30;
 
     public static WebElement waitForElementPresence(WebDriver driver, By by) {
         return new WebDriverWait(driver, Duration.ofSeconds(WAIT_DURATION_SEC)).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public static List<WebElement> waitForElementsPresence(WebDriver driver, By by) {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_DURATION_SEC)).until(ExpectedConditions.presenceOfElementLocated(by));
+        return driver.findElements(by);
     }
 
     public static WebElement waitForElementBeingClickable(WebDriver driver, By by) {
