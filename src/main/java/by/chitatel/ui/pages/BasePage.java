57c022a8d2342290d9cb9ca3001a555948d2dc37
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-26 14:51:03 +0200

    [ADD] Added methods to interact with top bar (search field, cart button)

--- src/main/java/by/chitatel/ui/pages/BasePage.java
+++ src/main/java/by/chitatel/ui/pages/BasePage.java
@@ -1,15 +1,44 @@
 package by.chitatel.ui.pages;
 
 import by.chitatel.ui.driver.DriverSingleton;
+import by.chitatel.ui.modals.SearchModal;
+import by.chitatel.ui.utils.Waiters;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
     protected WebDriver driver;
-    protected final String BASE_URL = "https://chitatel.by/password/reset";
+    private final By searchFieldBy = By.xpath("//input[@id='input-search']");
+    private final By cartButtonBy = By.xpath("//div[@class='h-cart']");
+    private final By cartCountBy = By.xpath("//div[@class='h-cart']//div[@class='count']");
 
     public BasePage() {
         this.driver = DriverSingleton.getWebDriver();
     }
 
     public abstract BasePage openPage();
+
+    public SearchModal typeIntoSearchField(String text){
+        Waiters.waitForElementPresence(driver, searchFieldBy).sendKeys(text);
+        return new SearchModal();
+    }
+
+    public CartPage clickOnCartButton(){
+        Waiters.waitForElementPresence(driver, cartButtonBy).click();
+        return new CartPage();
+    }
+
+    protected void waitForCartUpdate(String initialAmountOfItemsInCart){
+        while(getAmountOfItemsInCart().equals(initialAmountOfItemsInCart)){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    protected String getAmountOfItemsInCart(){
+        return Waiters.waitForElementPresence(driver, cartCountBy).getText();
+    }
 }
