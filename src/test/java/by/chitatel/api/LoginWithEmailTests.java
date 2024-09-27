70ec72a3b7b55352623e25f1eb8f7974fc3304c8
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-27 17:59:54 +0200

    [ADD] Added new test cases [REFACTOR] Removed hard-coded values

--- src/test/java/by/chitatel/api/LoginWithEmailTests.java
+++ src/test/java/by/chitatel/api/LoginWithEmailTests.java
@@ -5,51 +5,93 @@ import by.chitatel.api.responses.login.email.EmailLoginErrors;
 import by.chitatel.api.utils.Errors;
 import by.chitatel.api.utils.FormParameters;
 import by.chitatel.api.utils.Responses;
+import by.chitatel.constants.ErrorMessages;
+import by.chitatel.generators.Emails;
+import by.chitatel.generators.Passwords;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Random;
 
 public class LoginWithEmailTests extends BaseTest {
 
     @Test
     public void testLoginEmailOptions() {
-        Response response = new LoginWithEmail().performOptionsRequest(csrfToken,cookies);
+        Response response = new LoginWithEmail().performOptionsRequest(csrfToken, cookies);
         String allowedMethods = response.getHeader("Allow");
 
         Assertions.assertEquals("GET,HEAD,POST", allowedMethods);
     }
 
     @Test
+    public void testGetRequest() {
+        String email = Emails.generateValidEmail();
+        String password = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
+        Response response = new LoginWithEmail().performGetRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+
+        Assertions.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void testHeadRequest() {
+        String email = Emails.generateValidEmail();
+        String password = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
+        Response response = new LoginWithEmail().performHeadRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+
+        Assertions.assertEquals(statusCode, 200);
+    }
+
+    @Test
     public void testLoginWithIncorrectEmailAndPassword() {
-        Map<String, Object> formParams = new FormParameters().setEmail("user@email.com").setPassword("123456").setRememberMe("1").build();
+        String email = Emails.generateValidEmail();
+        String password = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
         Response response = new LoginWithEmail().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
 
         Assertions.assertEquals(statusCode, 200);
-        Assertions.assertEquals("Неправильный e-mail или пароль!", errors.getNoUserError());
+        Assertions.assertEquals(ErrorMessages.EMAIL_AND_PASSWORD_ARE_WRONG, errors.getNoUserError());
         Assertions.assertNull(errors.getEmailError());
         Assertions.assertNull(errors.getPasswordError());
     }
 
     @Test
     public void testLoginWithEmptyParameters() {
-        Map<String, Object> formParams = new FormParameters().setEmail("").setPassword("").setRememberMe("").build();
+        String email = "";
+        String password = "";
+        String rememberMe = "";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
         Response response = new LoginWithEmail().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
 
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNoUserError());
-        Assertions.assertEquals("Вы не указали \"Email\"", errors.getEmailError().getFirst());
-        Assertions.assertEquals("Вы не указали \"Пароль\"", errors.getPasswordError().getFirst());
+        Assertions.assertEquals(ErrorMessages.EMAIL_WAS_NOT_INPUT, errors.getEmailError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
     }
 
     @Test
     public void testLoginWithIncorrectEmailAndEmptyPassword() {
-        Map<String, Object> formParams = new FormParameters().setEmail("user@test.com").setPassword("").setRememberMe("0").build();
+        String email = Emails.generateValidEmail();
+        String password = "";
+        String rememberMe = "0";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
         Response response = new LoginWithEmail().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
@@ -57,19 +99,23 @@ public class LoginWithEmailTests extends BaseTest {
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNoUserError());
         Assertions.assertNull(errors.getEmailError());
-        Assertions.assertEquals("Вы не указали \"Пароль\"", errors.getPasswordError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
     }
 
     @Test
     public void testLoginWithEmptyEmailAndIncorrectPassword() {
-        Map<String, Object> formParams = new FormParameters().setEmail("").setPassword("123456").setRememberMe("").build();
+        String email = "";
+        String password = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
         Response response = new LoginWithEmail().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
 
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNoUserError());
-        Assertions.assertEquals("Вы не указали \"Email\"", errors.getEmailError().getFirst());
+        Assertions.assertEquals(ErrorMessages.EMAIL_WAS_NOT_INPUT, errors.getEmailError().getFirst());
         Assertions.assertNull(errors.getPasswordError());
     }
 
@@ -82,19 +128,23 @@ public class LoginWithEmailTests extends BaseTest {
 
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNoUserError());
-        Assertions.assertEquals("Вы не указали \"Email\"", errors.getEmailError().getFirst());
-        Assertions.assertEquals("Вы не указали \"Пароль\"", errors.getPasswordError().getFirst());
+        Assertions.assertEquals(ErrorMessages.EMAIL_WAS_NOT_INPUT, errors.getEmailError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
     }
 
     @Test
     public void testLoginWithIntegerParameters() {
-        Map<String, Object> formParams = new FormParameters().setEmail(123456).setPassword(123456).setRememberMe(1).build();
+        int email = new Random().nextInt(1000000);
+        int password = new Random().nextInt(1000000);
+        int rememberMe = new Random().nextInt(1000000);
+
+        Map<String, Object> formParams = new FormParameters().setEmail(email).setPassword(password).setRememberMe(rememberMe).build();
         Response response = new LoginWithEmail().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
 
         Assertions.assertEquals(statusCode, 200);
-        Assertions.assertEquals("Неправильный e-mail или пароль!", errors.getNoUserError());
+        Assertions.assertEquals(ErrorMessages.EMAIL_AND_PASSWORD_ARE_WRONG, errors.getNoUserError());
         Assertions.assertNull(errors.getEmailError());
         Assertions.assertNull(errors.getPasswordError());
     }
@@ -107,7 +157,7 @@ public class LoginWithEmailTests extends BaseTest {
         EmailLoginErrors errors = Errors.getErrorsFromEmailLoginResponse(response);
 
         Assertions.assertEquals(statusCode, 200);
-        Assertions.assertEquals("Неправильный e-mail или пароль!", errors.getNoUserError());
+        Assertions.assertEquals(ErrorMessages.EMAIL_AND_PASSWORD_ARE_WRONG, errors.getNoUserError());
         Assertions.assertNull(errors.getEmailError());
         Assertions.assertNull(errors.getPasswordError());
     }
@@ -120,7 +170,7 @@ public class LoginWithEmailTests extends BaseTest {
 
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNoUserError());
-        Assertions.assertEquals("Вы не указали \"Email\"", errors.getEmailError().getFirst());
-        Assertions.assertEquals("Вы не указали \"Пароль\"", errors.getPasswordError().getFirst());
+        Assertions.assertEquals(ErrorMessages.EMAIL_WAS_NOT_INPUT, errors.getEmailError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
     }
 }
--- src/test/java/by/chitatel/api/LoginWithPhoneTests.java
+++ src/test/java/by/chitatel/api/LoginWithPhoneTests.java
@@ -4,26 +4,67 @@ import by.chitatel.api.endpoints.LoginWithPhone;
 import by.chitatel.api.responses.login.phone.PhoneLoginErrors;
 import by.chitatel.api.utils.Errors;
 import by.chitatel.api.utils.FormParameters;
-import by.chitatel.api.utils.Responses;
+import by.chitatel.constants.ErrorMessages;
+import by.chitatel.generators.Passwords;
+import by.chitatel.generators.Phones;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Random;
 
 public class LoginWithPhoneTests extends BaseTest {
 
     @Test
     public void testLoginPhoneOptions() {
-        Response response = new LoginWithPhone().performOptionsRequest(csrfToken,cookies);
+        Response response = new LoginWithPhone().performOptionsRequest(csrfToken, cookies);
         String allowedMethods = response.getHeader("Allow");
 
         Assertions.assertEquals("POST", allowedMethods);
     }
 
     @Test
-    public void testChitatelPhoneLogin() {
-        Map<String, Object> formParams = new FormParameters().setPhone("+375(99)1111111").setPhonePassword("test").setPhoneRememberMe("1").build();
+    public void testPhoneLoginWithValidNotFormattedPhone() {
+        String phoneNumber = Phones.generateIncorrectPhoneNumber().getFullPhoneNumber();
+        String phonePassword = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertEquals(ErrorMessages.PHONE_AND_PASSWORD_ARE_WRONG, errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertNull(errors.getPasswordError());
+    }
+
+    @Test
+    public void testPhoneLoginWithPhoneThatDoesNotHaveCodes() {
+        String phoneNumber = Phones.generateIncorrectPhoneNumber().getPhoneNumber();
+        String phonePassword = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertEquals(ErrorMessages.PHONE_AND_PASSWORD_ARE_WRONG, errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertNull(errors.getPasswordError());
+    }
+
+    @Test
+    public void testPhoneLoginWithValidPhoneAndShortPassword() {
+        String phoneNumber = Phones.generateIncorrectPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = Passwords.generatePassword(5);
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
         Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
         int statusCode = response.statusCode();
         PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
@@ -31,6 +72,163 @@ public class LoginWithPhoneTests extends BaseTest {
         Assertions.assertEquals(statusCode, 200);
         Assertions.assertNull(errors.getNouserError());
         Assertions.assertNull(errors.getPhoneError());
-        Assertions.assertEquals("The password phone must be at least 6 characters.", errors.getPasswordError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_IS_TOO_SHORT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithValidPhoneAndLongPassword() {
+        String phoneNumber = Phones.generateIncorrectPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = Passwords.generatePassword(500);
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_IS_TOO_LONG, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithMockPhoneAndIncorrectPassword() {
+        String phoneNumber = Phones.generateMockPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = Passwords.generatePassword();
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertEquals(ErrorMessages.PASSWORD_DOES_NOT_MATCH, errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertNull(errors.getPasswordError());
+    }
+
+    @Test
+    public void testPhoneLoginWithMockPhoneAndEmptyPassword() {
+        String phoneNumber = Phones.generateMockPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = "";
+        String rememberMe = "1";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithEmptyParams() {
+        String phoneNumber = "";
+        String phonePassword = "";
+        String rememberMe = "";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertEquals(ErrorMessages.PHONE_NUMBER_WAS_NOT_INPUT, errors.getPhoneError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithBooleanParams() {
+        boolean phoneNumber = true;
+        boolean phonePassword = true;
+        boolean rememberMe = false;
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_IS_TOO_SHORT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithIntegerParams() {
+        int phoneNumber = new Random().nextInt(1000000);
+        int phonePassword = new Random().nextInt(1000000);
+        int rememberMe = new Random().nextInt(1000000);
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertEquals(ErrorMessages.PHONE_AND_PASSWORD_ARE_WRONG, errors.getNouserError());
+        Assertions.assertNull(errors.getPhoneError());
+        Assertions.assertNull(errors.getPasswordError());
+    }
+
+    @Test
+    public void testPhoneLoginWithNullParams() {
+        String phoneNumber = null;
+        String phonePassword = null;
+        String rememberMe = null;
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertEquals(ErrorMessages.PHONE_NUMBER_WAS_NOT_INPUT, errors.getPhoneError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithoutParams() {
+        Response response = new LoginWithPhone().performPostRequest(csrfToken, cookies);
+        int statusCode = response.statusCode();
+        PhoneLoginErrors errors = Errors.getErrorsFromPhoneLoginResponse(response);
+
+        Assertions.assertEquals(statusCode, 200);
+        Assertions.assertNull(errors.getNouserError());
+        Assertions.assertEquals(ErrorMessages.PHONE_NUMBER_WAS_NOT_INPUT, errors.getPhoneError().getFirst());
+        Assertions.assertEquals(ErrorMessages.PASSWORD_WAS_NOT_INPUT, errors.getPasswordError().getFirst());
+    }
+
+    @Test
+    public void testPhoneLoginWithoutToken() {
+        String phoneNumber = Phones.generateMockPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = Passwords.generatePassword();
+        String rememberMe = "0";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, cookies);
+        int statusCode = response.statusCode();
+
+        Assertions.assertEquals(statusCode, 419);
+    }
+
+    @Test
+    public void testPhoneLoginWithoutCookies() {
+        String phoneNumber = Phones.generateMockPhoneNumber().getFullPhoneNumberFormatted();
+        String phonePassword = Passwords.generatePassword();
+        String rememberMe = "0";
+
+        Map<String, Object> formParams = new FormParameters().setPhone(phoneNumber).setPhonePassword(phonePassword).setPhoneRememberMe(rememberMe).build();
+        Response response = new LoginWithPhone().performPostRequest(formParams, csrfToken);
+        int statusCode = response.statusCode();
+
+        Assertions.assertEquals(statusCode, 419);
     }
 }
