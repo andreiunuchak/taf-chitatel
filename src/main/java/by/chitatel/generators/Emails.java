bdc9b03900d5044514cb96eb45a552eeb3743e5c
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-10-02 13:23:51 +0200

    [REFACTOR] Removed comments

--- src/main/java/by/chitatel/generators/Emails.java
+++ src/main/java/by/chitatel/generators/Emails.java
@@ -39,24 +39,24 @@ public class Emails {
 
     public static String generateInvalidEmail() {
         Random random = new Random();
-        String invalidLocalPart = generateRandomString(INVALID_LOCAL_CHARS, random.nextInt(5) + 1); // Длина от 1 до 5 символов
-        String invalidDomainPart = generateRandomString(INVALID_DOMAIN_CHARS, random.nextInt(5) + 1); // Длина от 1 до 5 символов
-        String invalidTopLevelDomain = generateRandomString(INVALID_DOMAIN_CHARS, random.nextInt(2) + 1); // Длина от 1 до 2 символов
+        String invalidLocalPart = generateRandomString(INVALID_LOCAL_CHARS, random.nextInt(5) + 1);
+        String invalidDomainPart = generateRandomString(INVALID_DOMAIN_CHARS, random.nextInt(5) + 1);
+        String invalidTopLevelDomain = generateRandomString(INVALID_DOMAIN_CHARS, random.nextInt(2) + 1);
 
         // Формирование некорректного email с разными ошибками
         String[] invalidEmails = {
-                invalidLocalPart + "@" + invalidDomainPart + "." + invalidTopLevelDomain, // Некорректные символы в обеих частях
-                invalidLocalPart + "@example.com",  // Некорректная локальная часть
-                "user@" + invalidDomainPart + ".com", // Некорректный домен
-                "user@example." + invalidTopLevelDomain, // Некорректный TLD
-                "user@.com",  // Домен начинается с точки
-                ".user@example.com", // Локальная часть начинается с точки
-                "user@com", // Доменная часть без точки
-                "user@example..com", // Две точки подряд в доменной части
-                "user@.example.com", // Точка в начале доменной части
-                "user@example.com.", // Точка в конце доменной части
-                "@example.com", // Отсутствие локальной части
-                "user@" // Отсутствие доменной части
+                invalidLocalPart + "@" + invalidDomainPart + "." + invalidTopLevelDomain,
+                invalidLocalPart + "@example.com",
+                "user@" + invalidDomainPart + ".com",
+                "user@example." + invalidTopLevelDomain,
+                "user@.com",
+                ".user@example.com",
+                "user@com",
+                "user@example..com",
+                "user@.example.com",
+                "user@example.com.",
+                "@example.com",
+                "user@"
         };
 
         return invalidEmails[random.nextInt(invalidEmails.length)];
